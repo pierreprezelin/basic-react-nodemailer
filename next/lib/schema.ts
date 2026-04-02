8b96277123299743db8416ed08882eee3ba0b12d
Pierre Prezelin <pierre.prezelin@ammareal.fr>
2026-04-02 16:39:49 -0400

    Update error message in case of no email

--- next/lib/schema.ts
+++ next/lib/schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const EmailSchema = z.object({
-	email: z.string().min(1, "L'email est requis").email("Le format de l'email est invalide."),
+	email: z.string().min(1, "Une adresse email est requise.").email("Le format de l'email est invalide."),
 });
 
 export type EmailInput = z.infer<typeof EmailSchema>;
