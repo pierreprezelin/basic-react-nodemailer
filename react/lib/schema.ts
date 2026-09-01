c1a7ba30ea1e96d3acda5241073c869a4f72c42f
Pierre Prézelin <pierreprezelin.pro@gmail.com>
2026-09-01 09:54:21 +0200

    Translate schema responses in English

--- react/lib/schema.ts
+++ react/lib/schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const EmailSchema = z.object({
-	email: z.string().min(1, "Une adresse email est requise.").email("Le format de l'email est invalide."),
+	email: z.string().min(1, "An email address is required.").email("Email format is invalid."),
 });
 
 export type EmailInput = z.infer<typeof EmailSchema>;
